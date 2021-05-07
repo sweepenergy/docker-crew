12cbe0a68f33266ccb6f39842df387acd2e6d055
jakobpotosme <jakobpotosme@icloud.com>
2021-05-06 23:01:41 -0700

    Changes that made demo video easier to follow

--- routes/sweepapiStream.js
+++ routes/sweepapiStream.js
@@ -64,7 +64,7 @@ module.exports = function(app) {
 
         // stream/5b6efd8a-cd1d-4bc2-82b7-d410579b1787/ts/current_b/dataset
         //currently on listing data as span = 1 hour, time scale = 1 month
-        axios.get(sweepAPI.url + "stream/" + id + "/ts/" + var_name + "/dataset?span=1h&time_scale=1m&range_start=2018-01-01T08:00:00.000Z&range_end=2021-04-24T05:14:57.027Z&limit=&ts_type=avg", sweepAPI.config)
+        axios.get(sweepAPI.url + "stream/" + id + "/ts/" + var_name + "/dataset?span=1m&time_scale=1d&range_start=2021-05-06T05:00:00.000Z&range_end=2021-05-07T04:34:07.000Z&limit=&ts_type=avg", sweepAPI.config)
             .then(function(response){
                 // console.log(response.data);
                 res.render("streamDataSet", {
